d3f895d2d04a0402103c5e7de2a98aa896730354
RoboCopGay <vinicios.sousa909@gmail.com>
2020-04-10 01:15:45 -0300

    adding a rgb counter

--- image_to_rgb.py
+++ image_to_rgb.py
@@ -1,6 +1,6 @@
 from PIL import Image
 
-def get_color_matrix(img)
+def get_color_matrix(img):
     color_list = []
     for x in range(img.width):
         color_list.append([])
@@ -13,4 +13,14 @@ if __name__ == '__main__':
     from sys import argv as args
 
     img = Image.open(args[1])
-    print(get_color_matrix(img))
+
+    history = {}
+    color_matriz = get_color_matrix(img)
+    for x in range(img.width):
+        for y in range(img.height):
+            for rgb in color_matriz[x][y]:
+                if rgb not in history:
+                    history[rgb] = 1
+                else:
+                    history[rgb] += 1
+
