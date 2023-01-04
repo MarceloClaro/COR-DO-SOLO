46edb7541c8362890409975c34447cc45c4741a2
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2023-01-04 03:22:13 -0300

    Update solo_segmentation.py

--- solo_segmentation.py
+++ solo_segmentation.py
@@ -1,63 +1,81 @@
 import streamlit as st
 import colorsys
 from sklearn.cluster import KMeans
-import gc
 from PIL import Image
 import numpy as np
 
-def rgb_to_munsell(center):
-    r,g,b = center[0],center[1],center[2]
+def rgb_to_munsell(r, g, b):
+    """Converte uma cor RGB para o sistema de cores Munsell.
+    
+    Parâmetros:
+    r (int): Valor de vermelho (0-255).
+    g (int): Valor de verde (0-255).
+    b (int): Valor de azul (0-255).
+    
+    Retorno:
+    str: Cor no sistema de cores Munsell.
+    """
     h, l, s = colorsys.rgb_to_hls(r/255.0, g/255.0, b/255.0)
     h = h*360
-    if h < 20:
-        hue = "R"
-    elif h < 40:
-        hue = "YR"
-    elif h < 75:
-        hue = "Y"
-    elif h < 155:
-        hue = "GY"
-    elif h < 190:
-        hue = "G"
-    elif h < 260:
-        hue = "GY"
-    elif h < 290:
-        hue = "G"
-    elif h < 335:
-        hue = "BG"
-    else:
-        hue = "B"
-    if l < 0.25:
-        value = "2.5"
-    elif l < 0.3:
-        value = "3"
-    elif l < 0.4:
-        value = "4"
-    elif l < 0.5:
-        value = "5"
-    elif l < 0.6:
-        value = "6"
-    elif l < 0.7:
-        value = "7"
-    elif l < 0.8:
-        value = "8"
-    else:
-        value = "10"
-    if s < 0.2:
-        chroma = "0.5"
-    elif s < 0.4:
-        chroma = "1"
-    elif s < 0.6:
-        chroma = "2"
-    elif s < 0.8:
-        chroma = "3"
-    else:
-        chroma = "4"
+    
+    hue_map = {
+        (0, 20): 'R',
+        (20, 40): 'YR',
+        (40, 75): 'Y',
+        (75, 155): 'GY',
+        (155, 190): 'G',
+        (190, 260): 'GY',
+        (260, 290): 'G',
+        (290, 335): 'BG',
+        (335, 360): 'B',
+    }
+    for hue_range, hue_name in hue_map.items():
+        if hue_range[0] <= h < hue_range[1]:
+            hue = hue_name
+            break
+    
+    value_map = {
+        (0, 0.25): '2.5',
+        (0.25, 0.3): '3',
+        (0.3, 0.4): '4',
+        (0.4, 0.5): '5',
+        (0.5, 0.6): '6',
+        (0.6, 0.7): '7',
+        (0.7, 0.8): '8',
+        (0.8, 1): '10',
+    }
+    for value_range, value_name in value_map.items():
+        if value_range[0] <= l < value_range[1]:
+            value = value_name
+            break
+    
+    chroma_map = {
+        (0, 0.2): '0.5',
+        (0.2, 0.4): '1',
+        (0.4, 0.6): '2',
+        (0.6, 0.8): '3',
+        (0.8, 1): '4',
+    }
+    for chroma_range, chroma_name in chroma_map.items():
+        if chroma_range[0] <= s < chroma_range[1]:
+            chroma = chroma_name
+            break
+    
     return f"{hue} {value}/{chroma}"
 
-def classificar_cor_solo(img_path, largura, altura, k):
-    # Carregar imagem e redimensioná-la
-    img = Image.open(img_path)
+def classificar_cor_solo(img, largura, altura, k):
+    """Classifica as cores dominantes de uma imagem de solo.
+    
+    Parâmetros:
+    img (PIL.Image): Imagem a ser classificada.
+    largura (int): Largura da imagem redimensionada.
+    altura (int): Altura da imagem redimensionada.
+    k (int): Número de clusters a serem gerados pelo algoritmo K-Means.
+    
+    Retorno:
+    dict: Porcentagem de pixels de cada cor classificada.
+    """
+    # Redimensionar imagem
     img = img.resize((largura, altura))
     img = np.array(img)
     # Aplicar k-means para segmentação da imagem
@@ -65,34 +83,32 @@ def classificar_cor_solo(img_path, largura, altura, k):
     # Obter as cores dominantes
     cores = kmeans.cluster_centers_.astype(int)
     # Convertê-las para o sistema de cores Munsell
-    cores_munsell = [rgb_to_munsell(cor) for cor in cores]
+    cores_munsell = [rgb_to_munsell(cor[0], cor[1], cor[2]) for cor in cores]
     # Contar a quantidade de pixels de cada cor
     color_counts = {}
     for i in range(k):
         color_counts[cores_munsell[i]] = len(np.where(kmeans.labels_ == i)[0])
     # Calcular as porcentagens de cada cor
-# Calcular as porcentagens de cada cor
-total_pixels = sum(color_counts.values())
-color_percentages = {color: count / total_pixels * 100 for color, count in color_counts.items()}
-# Liberar espaço na RAM
-del img, kmeans, cores, color_counts
-gc.collect()
-return color_percentages
+    total_pixels = sum(color_counts.values())
+    color_percentages = {color: count / total_pixels * 100 for color, count in color_counts.items()}
+    return color_percentages
 
 def main():
     st.title('Classificação da Cor do Solo')
     st.subheader('Carta de cor de solo Munsell')
+    
     menu = st.sidebar.selectbox('Selecione uma opção', ['O que é solo?', 'O que é Carta de classificação de cor de Munsell e porque a cor do solo é importante?', 'Como proceder e analisar cor de solo, segundo a Embrapa', 'Classificação da Cor do Solo'])
     if menu == 'Classificação da Cor do Solo':
-        st.markdown('Selecione uma imagem para classificar a cor do solo.')
-        img_path = st.file_uploader('Imagem', type='jpg')
-   if img_path is not None:
-    largura = st.number_input('Largura', min_value=1, max_value=1000, value=500)
-    altura = st.number_input('Altura', min_value=1, max_value=1000, value=500)
-    k = st.number_input('Número de clusters', min_value=1, max_value=20, value=5)
-    color_percentages = classificar_cor_solo(img_path, largura, altura, k)
-    st.bar_chart(color_percentages)
-    
-if name == 'main':
-main()
-
+        st.markdown('Selecione uma imagem de solo para classificar as cores dominantes:')
+        img_file = st.file_uploader('', type=['jpg', 'jpeg', 'png'])
+        if img_file is not None:
+            with Image.open(img_file) as img:
+                largura = st.sidebar.slider('Largura da imagem redimensionada', 100, 1000, 500, 100)
+                altura = st.sidebar.slider('Altura da imagem redimensionada', 100, 1000, 500, 100)
+                k = st.sidebar.slider('Número de clusters', 2, 20, 10, 1)
+                color_percentages = classificar_cor_solo(img, largura, altura, k)
+                st.markdown(f'Cores dominantes na imagem:')
+                for color, percentage in color_percentages.items():
+                    st.markdown(f'{color}: {percentage:.2f}%')
+if __name__ == '__main__':
+    main()
