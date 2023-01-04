b94526719db03d85fbd95693b9c903268f15177f
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2023-01-04 11:38:50 -0300

    Update solo_segmentation.py

--- solo_segmentation.py
+++ solo_segmentation.py
@@ -69,7 +69,7 @@ def classificar_cor_solo(img, largura, altura, k):
 
 def main():
     st.title("CLASSIFICADOR DE COR DE SOLO - MUNSEL")
-    st.write("Solo é a camada mais superficial da Terra, composta principalmente de rochas e minerais fragmentados, matéria orgânica, água e ar. A cor do solo é um importante indicador de suas características físisicas e químicas, pois reflete a presença de diferentes componentes e nutrientes. O estudo da cor do solo é importante em diversas áreas, como agricultura, geologia e meio ambiente.")
+    st.write("Solo é a camada mais superficial da Terra, composta principalmente de rochas e minerais fragmentados, matéria orgânica, água e ar. A cor do solo é um importante indicador de suas características físicas e químicas, pois reflete a presença de diferentes componentes e nutrientes. O estudo da cor do solo é importante em diversas áreas, como agricultura, geologia e meio ambiente.")
 st.write("Para classificar as cores do solo, utilizamos o sistema de cores Munsell. O sistema de cores Munsell é um método padronizado para descrever cores baseado em três fatores: tonalidade (matiz), valor (brilho) e croma (saturação). Esses três fatores são combinados em uma notação que indica a cor específica, por exemplo, '5Y 7/4' significa uma cor de tonalidade amarela, valor 7 e croma 4.")
 st.write("Neste aplicativo, você pode carregar uma imagem de solo e visualizar as cores dominantes na imagem. Escolha o número de clusters para o agrupamento da imagem e clique em 'Classificar cores' para ver os resultados.")
 uploaded_file = st.file_uploader("Escolha a imagem de solo:", type="jpg")
@@ -83,3 +83,20 @@ if uploaded_file is not None:
     df = pd.DataFrame(list(cores.items()), columns=['Cor', 'Porcentagem de pixels'])
     df['Porcentagem de pixels'] = df['Porcentagem de pixels'] / df['Porcentagem de pixels'].sum()
     st.bar_chart(df)
+     uploaded_file = st.file_uploader("Escolha a imagem de solo:", type=["png", "jpg", "jpeg"])
+    margem_erro = st.sidebar.slider("Margem de erro para classificação de cor (em %):", 0, 50, 10)
+
+    if st.sidebar.button("Classificar cores"):
+        cores = classificar_cor_solo(image, largura, altura, k)
+        total_pixels = largura * altura
+cores = classificar_cor_solo(image, largura, altura, k)
+munsell_labels = []
+munsell_values = []
+for munsell, count in cores.items():
+munsell_labels.append(munsell)
+munsell_values.append(count / total_pixels)
+st.bar_chart(munsell_values, munsell_labels)
+
+if name == "main":
+main()
+   
