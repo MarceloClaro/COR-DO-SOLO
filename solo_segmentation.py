f88668ca2bada934134fec896c70c695e2d636b6
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2023-01-04 13:22:27 -0300

    Update solo_segmentation.py

--- solo_segmentation.py
+++ solo_segmentation.py
@@ -5,10 +5,17 @@ import cv2
 import colorsys
 from sklearn.cluster import KMeans
 
+# Função para converter uma cor RGB para a notação de cor Munsell
 def rgb_to_munsell(center):
-    r,g,b = center[0],center[1],center[2]
+    r, g, b = center[0], center[1], center[2]
+    
+    # Converter cor RGB para espaço de cor HLS
     h, l, s = colorsys.rgb_to_hls(r/255.0, g/255.0, b/255.0)
-    h = h*360
+    
+    # Multiplicar h por 360 para obter o ângulo na escala HSL
+    h = h * 360
+    
+    # Verificar em qual faixa de ângulo o h se encontra e atribuir a tonalidade correspondente
     if h < 20:
         hue = "R"
     elif h < 40:
@@ -20,13 +27,15 @@ def rgb_to_munsell(center):
     elif h < 190:
         hue = "G"
     elif h < 260:
-        hue = "BG"
+        hue = "BG" 
     elif h < 290:
         hue = "B"
     elif h < 335:
         hue = "PB"
     else:
         hue = "P"
+    
+    # Verificar em qual faixa o l se encontra e atribuir o valor correspondente
     if l < 0.25:
         value = "2.5"
     elif l < 0.3:
@@ -43,6 +52,8 @@ def rgb_to_munsell(center):
         value = "8"
     else:
         value = "10"
+    
+    # Verificar em qual faixa o s se encontra e atribuir o croma correspondente
     if s < 0.2:
         chroma = "0.5"
     elif s < 0.4:
@@ -53,58 +64,63 @@ def rgb_to_munsell(center):
         chroma = "3"
     else:
         chroma = "4"
+    
+    # Retornar a notação de cor Munsell
     return f"{hue} {value}/{chroma}"
 
-@st.cache(allow_output_mutation=True)
+# Função para classificar as cores de uma imagem de solo
 def classificar_cor_solo(img, largura, altura, k):
+    # Redimensionar imagem
     img = cv2.resize(img, (largura, altura))
+    
+    # Converter imagem para o espaço de cor HSV
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    
+    # Aplicar agrupamento K-Means à imagem
     kmeans = KMeans(n_clusters=k, random_state=0).fit(img.reshape((largura * altura, 3)))
     labels = kmeans.labels_
+    
+    # Criar dicionário para armazenar as cores e a quantidade de pixels de cada cor
     cores = {}
+    
+    # Loop pelos clusters
     for i in range(k):
+        # Obter o centro do cluster
         center = kmeans.cluster_centers_[i]
+        
+        # Converter o centro do cluster para a notação de cor Munsell
         munsell = rgb_to_munsell(center)
+        
+        # Adicionar cor e quantidade de pixels ao dicionário
         cores[munsell] = (labels == i).sum()
+    
+    # Retornar dicionário de cores e quantidades de pixels
     return cores
 
 def main():
-  st.title("CLASSIFICADOR DE COR DE SOLO - MUNSEL")
-  st.write("Solo é a camada mais superficial da Terra, composta principalmente de rochas e minerais fragmentados, matéria orgânica, água e ar. A cor do solo é um importante indicador de suas características físisicas e químicas, pois reflete a presença de diferentes componentes e nutrientes. O estudo da cor do solo é importante em diversas áreas, como agricultura, geologia e meio ambiente.")
-  st.write("Para classificar as cores do solo, utilizamos o sistema de cores Munsell. O sistema de cores Munsell é um método padronizado para descrever cores baseado em três fatores: tonalidade (matiz), valor (brilho) e croma (saturação). Esses três fatores são combinados em uma notação que indica a cor específica, por exemplo, '5Y 7/4' significa uma cor de tonalidade amarela, valor 7 e croma 4.")
-  st.write("Neste aplicativo, você pode carregar uma imagem de solo e visualizar as cores dominantes na imagem. Escolha o número de clusters para o agrupamento da imagem e clique em 'Classificar cores' para ver os resultados.")
-  uploaded_file = st.file_uploader("Escolha a imagem de solo:", type="jpg")
-  if uploaded_file is not None:
-    if os.path.exists(uploaded_file):
-      image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
-    else:
-      st.write("Arquivo não encontrado!")
+    st.title("CLASSIFICADOR DE COR DE SOLO - MUNSEL")
+    st.write("Solo é a camada mais superficial da Terra, composta principalmente de rochas e minerais fragmentados, matéria orgânica, água e ar. A cor do solo é um importante indicador de suas características físisicas e químicas, pois reflete a presença de diferentes componentes e nutrientes. O estudo da cor do solo é importante em diversas áreas, como agricultura, geologia e meio ambiente.")
+    st.write("Para classificar as cores do solo, utilizamos o sistema de cores Munsell. O sistema de cores Munsell é um método padronizado para descrever cores baseado em três fatores: tonalidade (matiz), valor (brilho) e croma (saturação). Esses três fatores são combinados em uma notação que indica a cor específica, por exemplo, '5Y 7/4' significa uma cor de tonalidade amarela, valor 7 e croma 4.")
+    st.write("Neste aplicativo, você pode carregar uma imagem de solo e visualizar as cores dominantes na imagem. Escolha o número de clusters para o agrupamento da imagem e clique em 'Classificar cores' para ver os resultados.")
+    
+    # Permitir o upload de imagem pelo usuário
+    uploaded_file = st.file_uploader("Escolha a imagem de solo:", type="jpg")
+    
+    # Se o usuário tiver carregado uma imagem
     if uploaded_file is not None:
-        # Convert the file to an opencv image.
+                # Converta o arquivo em uma imagem opencv.
         image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
+        # Obter largura e altura da imagem redimensionada pelo usuário
         largura = st.slider("Largura da imagem redimensionada:", min_value=100, max_value=1000, value=300)
         altura = st.slider("Altura da imagem redimensionada:", min_value=100, max_value=1000, value=300)
-        k = st.slider("Número de clusters:", min_value=2, max_value=20, value=5)
+           # Obter número de clusters pelo usuário
+            k = st.slider("Número de clusters:", min_value=1, max_value=20, value=5)
+            # Obter margem de erro para classificação de cor pelo usuário
+            margem_erro = st.sidebar.slider("Margem de erro para classificação de cor (em %):", 0, 50, 10)
+
+        # Exibir as cores dominantes na imagem
         st.write("As cores dominantes na imagem são:")
         cores = classificar_cor_solo(image, largura, altura, k)
         df = pd.DataFrame(list(cores.items()), columns=['Cor', 'Porcentagem de pixels'])
         df['Porcentagem de pixels'] = df['Porcentagem de pixels'] / df['Porcentagem de pixels'].sum()
-        if uploaded_file is not None:
-            image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
-            largura = st.slider("Largura da imagem redimensionada:", min_value=100, max_value=1000, value=300)
-            altura = st.slider("Altura da imagem redimensionada:", min_value=100, max_value=1000, value=300)
-            k = st.slider("Número de clusters:", min_value=2, max_value=20, value=5)
-            margem_erro = st.sidebar.slider("Margem de erro para classificação de cor (em %):", 0, 50, 10)
-            st.write("As cores dominantes na imagem são:")
-            cores = classificar_cor_solo(image, largura, altura, k)
-            df = pd.DataFrame(list(cores.items()), columns=['Cor', 'Porcentagem de pixels'])
-            df['Porcentagem de pixels'] = df['Porcentagem de pixels'] / df['Porcentagem de pixels'].sum()
-            st.bar_chart(df)
-    if st.sidebar.button("Classificar cores"):
-            munsell_labels = []
-            munsell_values = []
-    for munsell, count in cores.items():
-      munsell_labels.append(munsell)
-      munsell_values.append(count / total_pixels)
-      st.bar_chart(munsell_values, munsell_labels)
-    if __name__ == "__main__":
-      main()
+        st.write(df)
