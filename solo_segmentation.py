9def0f21cd3e8f216640104006265ac71d69995a
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2023-01-04 07:17:27 -0300

    Update solo_segmentation.py

--- solo_segmentation.py
+++ solo_segmentation.py
@@ -4,113 +4,142 @@ from sklearn.cluster import KMeans
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2
 
-st.title('CLASSIFICADOR DE COR DE SOLO - MUNSELL')
+HUE_MAP = [
+    ('R', (0, 20)),
+    ('YR', (20, 40)),
+    ('Y', (40, 75)),
+    ('GY', (75, 155)),
+    ('G', (155, 190)),
+    ('GY', (190, 260)),
+    ('G', (260, 290)),
+    ('BG', (290, 335)),
+    ('B', (335, 360)),
+]
 
-tipo_solo = st.sidebar.radio('Escolha o tipo de solo', ('Úmido', 'Seco'))
+VALUE_MAP = [
+    ('2.5', (0, 0.25)),
+    ('3', (0.25, 0.3)),
+    ('4', (0.3, 0.4)),
+    ('5', (0.4, 0.5)),
+    ('6', (0.5, 0.6)),
+    ('7', (0.6, 0.7)),
+    ('8', (0.7, 0.8)),
+    ('10', (0.8, 1)),
+]
 
-# Pergunte ao usuário para fazer o upload da imagem da amostra de solo
-imagem_file = st.file_uploader('Selecione a imagem da amostra de solo', type='jpg')
+CHROMA_MAP = [
+    ('0.5', (0, 0.2)),
+    ('1', (0.2, 0.4)),
+    ('2', (0.4, 0.6)),
+    ('3', (0.6, 0.8)),
+    ('4', (0.8, 1)),
+]
 
-st.markdown('O solo é uma camada fina e complexa da Terra que sustenta a vida vegetal e muitos outros organismos. Ele é composto de minerais, matéria orgânica, água e ar, e é responsável por fornecer nutrientes e água para as plantas. A cor do solo é importante para o estudo geográfico porque pode indicar o tipo e a quantidade de nutrientes presentes no solo, bem como o pH e a capacidade de retenção de água.')
-
-def converte_munsell(r, g, b):
-  # Converte os valores RGB em matiz, saturação e valor (HSV)
-  matiz, saturacao, valor = cv2.cvtColor(np.uint8([[[b, g, r]]]), cv2.COLOR_BGR2HSV)[0][0]
+def rgb_to_munsell(r, g, b):
+    """Converte uma cor RGB para o sistema de cores Munsell.
     
-def converte_munsell(r, g, b):
-  # Converte os valores RGB em matiz, saturação e valor (HSV)
-  matiz, saturacao, valor = cv2.cvtColor(np.uint8([[[b, g, r]]]), cv2.COLOR_BGR2HSV)[0][0]
+    Parâmetros:
+    r (int): Valor de vermelho (0-255).
+    g (int): Valor de verde (0-255).
+    b (int): Valor de azul (0-255).
+    
+    Retorno:
+    str: Cor no sistema de cores Munsell.
+    """
+    h, l, s = colorsys.rgb_to_hls(r/255.0, g/255.0, b/255.0)
+    h = h*360
+    
+    for hue_name, hue_range in HUE_MAP:
+        if hue_range[0] <= h < hue_range[1]:
+            hue = hue_name
+            break
+    
+    for value_name, value_range in VALUE_MAP:
+        if value_range[0] <= l < value_range[1]:
+            value = value_name
+            break
+    
+    for chroma_name, chroma_range in CHROMA_MAP:
+        if chroma_range[0] <= s < chroma_range[1]:
+            chroma = chroma_name
+            break
+    
+    return f"{hue} {value}/{chroma}"
 
-  # Mapeia os valores de matiz, saturação e valor resultantes para a tonalidade, o valor e o croma Munsell correspondentes mais próximos
-  tonalidades = ['R', 'YR', 'Y', 'GY', 'G', 'BG', 'B', 'PB', 'P', 'RP']
-  valores = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-  cromas = ['1/8', '1/4', '3/8', '1/2', '5/8', '3/4', '7/8', '1', '1 1/8', '1 1/4', '1 3/8', '1 1/2', '1 5/8', '1 3/4', '1 7/8', '2']
-  tonalidade_munsell = tonalidades[int(matiz / 30)]
-  valor_munsell = valores[int((valor / 255) * 10)]
-  croma_munsell = cromas[int((saturacao / 255) * 16)]
+def classificar_cor_solo(img, largura, altura, k):
+    """Classifica as cores dominantes de uma imagem de solo.
+    
+    Parâmetros:
+    img (PIL.Image): Imagem a ser classificada.
+    largura (int): Largura da imagem redimensionada.
+    altura (int): Altura da imagem redimensionada.
+    k (int): Número de clusters a serem gerados pelo algoritmo K-Means.
+    
+    Retorno:
+    dict: Porcentagem de pixels de cada cor classificada.
+    """
+    # Redimensionar imagem
+    img = img.resize((largura, altura))
+    img = np.array(img)
+    # Aplicar k-means para segmentação da imagem
+    kmeans = KMeans(n_clusters=k, random_state=0).fit(img.reshape(-1,3))
+    # Obter as cores dominantes
+    cores, contagem = np.unique(kmeans.labels_, return_counts=True)
+    cores_dominantes = {}
+    for cor, count in zip(cores, contagem):
+        r, g, b = kmeans.cluster_centers_[cor]
+        munsell = rgb_to_munsell(int(r), int(g), int(b))
+        cores_dominantes[munsell] = count / len(kmeans.labels_)
+    return cores_dominantes
 
-  # Retorna a cor Munsell resultante
-  return tonalidade_munsell
+def calcular_margem_erro(pixels, cores):
+    """Calcula a margem de erro entre os pixels da imagem e os pixels de cada cor classificada.
+    
+    Parâmetros:
+    pixels (int): Número total de pixels da imagem.
+    cores (dict): Porcentagem de pixels de cada cor classificada.
+    
+    Retorno:
+    dict: Margem de erro de cada cor classificada.
+    """
+    margem_erro = {}
+    for cor, porcentagem in cores.items():
+        margem_erro[cor] = abs(pixels - porcentagem) / pixels
+    return margem_erro
 
-def classifica_cor_solo(imagem, largura, altura, num_clusters):
-  # Redimensiona a imagem
-  imagem = cv2.resize(imagem, (largura, altura))
+def imprimir_cor_dominante(cores):
+    """Imprime a cor dominante da imagem.
+    
+    Parâmetros:
+    cores (dict): Porcentagem de pixels de cada cor classificada.
+    """
+    cor_dominante = max(cores, key=cores.get)
+    print(f"A cor dominante da imagem é {cor_dominante}")
 
-  # Executa o clustering K-Means na imagem
-  imagem = np.float32(imagem)
-  criterio = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-  num_cores, labels, (centers) = cv2.kmeans(imagem, num_clusters, None, criterio, 10, cv2.KMEANS_RANDOM_CENTERS)
+def imprimir_grafico_margem_erro(margem_erro):
+    """Imprime um gráfico de barras com a margem de erro de cada cor classificada.
+    
+    Parâmetros:
+    margem_erro (dict): Margem de erro de cada cor classificada.
+    """
+    plt.bar(margem_erro.keys(), margem_erro.values())
+    plt.xlabel('Cores')
+    plt.ylabel('Margem de erro')
+    plt.title('Margem de erro por cor')
+    plt.show()
 
-  # Conta o número de pixels em cada cluster
-  contador_cores = np.zeros(num_clusters)
-  for label in labels:
-    contador_cores[label] += 1
+def imprimir_grafico_pizza(cores):
+    """Imprime um gráfico de pizza com a porcentagem de pixels de cada cor classificada.
+    
+    Parâmetros:
+    cores (dict): Porcentagem de pixels de cada cor classificada.
+    """
+    labels = cores.keys()
+    sizes = cores.values()
+    plt.pie(sizes, labels=labels)
+    plt.title('Porcentagem de pixels por cor')
+    plt.show()
 
-  # Converte os valores RGB dos centros de cluster em cores Munsell
-  cores = []
-  for center in centers:
-    r, g, b = center
-    cores.append(converte_munsell(r, g, b))
-
-  # Calcula as porcentagens de pixels em cada cor classificada
-  porcentagens = []
-  for contador in contador_cores:
-    porcentagem = (contador / len(labels)) * 100
-    porcentagens.append(porcentagem)
-
-  # Retorna um dicionário com as porcentagens de pixels em cada cor classificada
-  resultado = dict(zip(cores, porcentagens))
-  return resultado
-
-if tipo_solo == 'Úmido' and imagem_file is not None:
-  # Lê a imagem da amostra de solo úmida
-  imagem = cv2.imread(imagem_file)
-
-  # Classifica a cor do solo úmido e obtém o resultado em um dicionário
-  resultado = classifica_cor_solo(imagem, largura, altura, num_clusters)
-
- if tipo_solo == 'Úmido' and imagem_file is not None:
-  # Lê a imagem da amostra de solo úmida
-  imagem = cv2.imread(imagem_file)
-
-  # Classifica a cor do solo úmido e obtém o resultado em um dicionário
-  resultado = classifica_cor_solo(imagem, largura, altura, num_clusters)
-
-  # Obtém as cores classificadas e as porcentagens de pixels em cada cor
-  cores = list(resultado.keys())
-  porcentagens = list(resultado.values())
-
-  # Cria o gráfico de margem de erro e variância
-  plt.figure()
-  plt.title('Classificação de cor de solo - Munsell')
-  plt.xlabel('Cores')
-  plt.ylabel('Porcentagem de pixels')
-  plt.errorbar(cores, porcentagens, fmt='o', ecolor='k')
-  plt.xticks(rotation=90)
-  plt.ylim(0, max(porcentagens) + 5)
-  plt.tight_layout()
-
-  # Exibe o gráfico no Streamlit
-  st.pyplot()
-
- elif tipo_solo == 'Seco' and imagem_file is not None:
-  # Lê a imagem da amostra de solo seco
-  imagem = cv2.imread(imagem_file)
-
-  # Classifica a cor do solo seco e obtém o resultado em um dicionário
-  resultado = classifica_cor_solo(imagem, largura, altura, num_clusters)
-
-  # Obtém as cores classificadas e as porcentagens de pixels em cada cor
-  cores = list(resultado.keys())
-  porcentagens = list(resultado.values())
-
-  # Cria o gráfico de margem de erro e variância
-  plt.figure()
-  plt.title('Classificação de cores do solo seco')
-plt.bar(cores, porcentagens, yerr=resultado['variancias'], capsize=10)
-plt.ylabel('Porcentagem de pixels')
-plt.xticks(rotation=45)
-
- 
+if name == "main":
+main()
