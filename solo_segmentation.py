e468f12375409ebe979592aa9fafb6c5d349941c
MARCELO CLARO LARANJEIRA <58664974+MarceloClaro@users.noreply.github.com>
2023-05-06 16:32:49 -0300

    Update solo_segmentation.py

--- solo_segmentation.py
+++ solo_segmentation.py
@@ -1,3 +1,4 @@
+
 import streamlit as st
 import numpy as np
 import cv2
@@ -67,30 +68,30 @@ soil_dict = {
         "caracteristicas": "Textura média ou argilosa, boa fertilidade natural e alta capacidade de retenção de água.",
         "vegetacao_tipica": "Cerrado, Caatinga e Mata Atlântica",
         "cultivos_manejo": "Adequado para culturas perenes e anuais. Recomenda-se o uso de práticas conservacionistas e adubação equilibrada."
-},
-"10YR3/3": {
-"sistema_munsell": "10YR3/3",
-"solo_embrapa": "Neossolo Regolítico",
-"descricao": "Solos pouco desenvolvidos, geralmente rasos e localizados em áreas com relevo acentuado.",
-"caracteristicas": "Baixa fertilidade natural, baixa capacidade de retenção de água e alta susceptibilidade à erosão.",
-"vegetacao_tipica": "Caatinga e Cerrado",
-"cultivos_manejo": "Uso limitado para agricultura. Preferencialmente, deve ser preservado para conservação ambiental e recarga de aquíferos."
+    },
+    "10YR3/3": {
+            "sistema_munsell": "10YR3/3",
+    "solo_embrapa": "Neossolo Regolítico",
+    "descricao": "Solos pouco desenvolvidos, geralmente rasos e localizados em áreas com relevo acentuado.",
+    "caracteristicas": "Baixa fertilidade natural, baixa capacidade de retenção de água e alta susceptibilidade à erosão.",
+    "vegetacao_tipica": "Caatinga e Cerrado",
+    "cultivos_manejo": "Uso limitado para agricultura. Preferencialmente, deve ser preservado para conservação ambiental e recarga de aquíferos."
 },
 "2.5YR5/4": {
-"sistema_munsell": "2.5YR5/4",
-"solo_embrapa": "Cambissolo Háplico",
-"descricao": "Solos de coloração avermelhada, geralmente pouco profundos e com horizonte B incipiente.",
-"caracteristicas": "Textura média, fertilidade natural moderada e moderada capacidade de retenção de água.",
-"vegetacao_tipica": "Cerrado, Mata Atlântica e Floresta Amazônica",
-"cultivos_manejo": "Pode ser cultivado com culturas anuais e perenes, desde que sejam adotadas práticas conservacionistas e adubação adequada."
+    "sistema_munsell": "2.5YR5/4",
+    "solo_embrapa": "Cambissolo Háplico",
+    "descricao": "Solos de coloração avermelhada, geralmente pouco profundos e com horizonte B incipiente.",
+    "caracteristicas": "Textura média, fertilidade natural moderada e moderada capacidade de retenção de água.",
+    "vegetacao_tipica": "Cerrado, Mata Atlântica e Floresta Amazônica",
+    "cultivos_manejo": "Pode ser cultivado com culturas anuais e perenes, desde que sejam adotadas práticas conservacionistas e adubação adequada."
 },
 "7R/0": {
-"sistema_munsell": "7R/0",
-"solo_embrapa": "Luvissolo Crômico",
-"descricao": "Solos de coloração avermelhada a arroxeada, profundos e bem drenados.",
-"caracteristicas": "Textura argilosa, alta fertilidade natural e alta capacidade de retenção de água.",
-"vegetacao_tipica": "Mata Atlântica e Floresta Amazônica",
-"cultivos_manejo": "Adequado para culturas perenes e anuais. Recomenda-se o uso de práticas conservacionistas e adubação equilibrada."
+    "sistema_munsell": "7R/0",
+    "solo_embrapa": "Luvissolo Crômico",
+    "descricao": "Solos de coloração avermelhada a arroxeada, profundos e bem drenados.",
+    "caracteristicas": "Textura argilosa, alta fertilidade natural e alta capacidade de retenção de água.",
+    "vegetacao_tipica": "Mata Atlântica e Floresta Amazônica",
+    "cultivos_manejo": "Adequado para culturas perenes e anuais. Recomenda-se o uso de práticas conservacionistas e adubação equilibrada."
 }
 }
 
@@ -98,38 +99,38 @@ soil_dict = {
 st.title("Classificação de Solo")
 uploaded_file = st.file_uploader("Escolha a imagem", type="jpg")
 if uploaded_file is not None:
-  image = Image.open(uploaded_file)
-  st.image(image, caption='Imagem', use_column_width=True)
-  img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
+image = Image.open(uploaded_file)
+st.image(image, caption='Imagem', use_column_width=True)
+img = cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
-# Redimensionar a imagem para uma lista de pixels
+#Redimensionar a imagem para uma lista de pixels
 Z = img.reshape((-1,3))
 Z = np.float32(Z)
 
-# Definir os critérios, número de clusters(K) e aplicar k-means()
+#Definir os critérios, número de clusters(K) e aplicar k-means()
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 K = 1
 ret, label, center = cv2.kmeans(Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 center = np.uint8(center)
 
-# Calcular a margem de erro e o desvio padrão da clusterização
+#Calcular a margem de erro e o desvio padrão da clusterização
 mean_error, std_deviation = calculate_error_and_std_deviation(Z, center)
-# Converter a imagem para valores de 8 bits
+
+#Converter a imagem para valores de 8 bits
 res = center[label.flatten()]
 res2 = res.reshape((img.shape))
 
-# Exibir a imagem e a notação Munsell correspondente
+#Exibir a imagem e a notação Munsell correspondente
 fig, ax = plt.subplots()
 ax.imshow(res2)
 ax.set_title(rgb_to_embrapa_munsell(center[0][0], center[0][1], center[0][2]))
 ax.axis('off')
 st.pyplot(fig)
 
-embrapa_notation = rgb_to_embrapa_munsell(center[0][0], center[0][1], center[0][2])
+#embrapa_notation = rgb_to_embrapa_munsell(center[0][0], center[0][1], center[0][2])
 soil_type = soil_dict.get(embrapa_notation, "NÃO CADASTRADO")
 
 st.write("Tipo de solo correspondente:")
 st.write(soil_type)
 st.write("Margem de erro da clusterização: {:.2f}".format(mean_error))
 st.write("Desvio padrão da clusterização: {:.2f}".format(std_deviation))
-
